f898e5c43d80bffc37c6162dd9f7efe0955b6af5
polinaefw6464 <komlina_pr@mail.ru>
2025-10-20 18:07:49 +0700

    Add files via upload

--- 4.py
+++ 4.py
@@ -1,5 +1,7 @@
-x,y,n=map(int,input().split())
-doxod_r1=x*n
-doxod_r2=y*n//100
-doxod_k=y*n-doxod_r2*100
-print(doxod_r1+doxod_r2,'руб.', doxod_k,'коп.')
+print('Вы поедете на бал?')
+x=str(input())
+y=['да','нет']
+if x not in y:
+    print('верно')
+else:
+    print('неверно')
